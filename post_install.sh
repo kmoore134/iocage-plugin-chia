396c91f9366f062f781f7823e0b1e33e8501fa38
Kris Moore <kris@ixsystems.com>
2021-06-03 18:47:30 -0400

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -22,13 +22,26 @@ cd clvm_rs
 maturin develop --release
 pip install git+https://github.com/Chia-Network/clvm@use_clvm_rs
 
-cd /root/chia-blockchain
 
 # Hack(s)!
+cd /root/chia-blockchain
 sed -i '' 's|elif platform=="linux":|elif platform=="linux" or platform.startswith("freebsd"):|g' chia/util/keychain.py
 sed -i '' 's|cryptography==3.4.6|cryptography==3.3.2|g' setup.py
 
+# Moar Hacks!
+portsnap fetch extract
+cd /usr/ports/security/py-cryptography
+echo "DEFAULT_VERSIONS+=ssl=openssl python=3.7 python3=3.7" >> /etc/make.conf
+make
+
+# Even mooaarr hacks!
+cp -R /usr/ports/security/py-cryptography/work-py37/stage/usr/local/lib/python3.7/site-packages/cryptography ${VIRTUAL_ENV}/lib/python3.7/site-packages/cryptography
+cp -R /usr/ports/security/py-cryptography/work-py37/stage/usr/local/lib/python3.7/site-packages/cryptography-3.3.2-py3.7.egg-info ${VIRTUAL_ENV}/lib/python3.7/site-packages/cryptography-3.3.2-py3.7.egg-info
+
+make clean
+
 # Install Chia
+cd /root/chia-blockchain
 sh install.sh
 
 # Init Chia and done!
